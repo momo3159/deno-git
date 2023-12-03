e2abcfe74eb3621941ad20aea161f37dfd80b085
tommy <tommydsha@gmail.com>
2023-12-03 15:39:20 +0900

    deno-cliffyを導入

--- main.ts
+++ main.ts
@@ -1,12 +1,18 @@
 import { clientFactory } from "./client.ts";
+import { Command, EnumType } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 
-export function add(a: number, b: number): number {
-  return a + b;
-}
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
   const client = clientFactory();
-  console.log(client)
+
+  await new Command()
+    .name("mygit")
+    .version("0.1.0")
+    .description("自作Git")
+    .action(() => { console.log("main") })
+    .command("log", "logコマンド")
+    .action(() => { console.log('git log'); console.log(client)})
+    .parse(Deno.args)
 }
 
 
